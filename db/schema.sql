f0662d4380706079796ea552d41aa7793a7252ed
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-08 10:37:25 -0700

    update database column personal_rating to null

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE media
     id int NOT NULL AUTO_INCREMENT,
     media_name varchar(255) NOT NULL,
     viewed BOOLEAN DEFAULT false,
-    personal_rating DECIMAL (10,2) NOT NULL,
+    personal_rating DECIMAL (10,2) NULL,
     PRIMARY KEY (id)
 );
