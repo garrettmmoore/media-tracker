4da00ca8ca799b6ad8bedf3965bbfeafc7062141
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-08 00:30:45 -0700

    bootstrap card update

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,44 @@
+// // Set up MySQL connection.
+// var mysql = require("mysql");
+
+// var connection;
+
+// if(process.env.JAWSDB_URL){
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }else {
+//     connection = mysql.createConnection({
+//         port: 3000,
+//         host: "localhost",
+//         user: "root",
+//         password: "Pugluverg51",
+//         database: "media_db"
+//     });
+// };
+
+// connection.connect();
+
+// // Export connection for our ORM to use
+// module.exports = connection;
+
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection;
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "Pugluverg51",
+  database: "media_db"
+});
 
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else {
-    connection = mysql.createConnection({
-        port: 3000,
-        host: "localhost",
-        user: "root",
-        password: "Pugluverg51",
-        database: "media_db"
-    });
-};
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-connection.connect();
-
-// Export connection for our ORM to use
+// Export connection for our ORM to use.
 module.exports = connection;
