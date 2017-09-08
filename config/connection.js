161490b9012396361221adf4bbcb2e7e5ea28765
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-08 10:29:49 -0700

    header font change

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if(process.env.JAWSDB_URL){
         port: 3000,
         host: "localhost",
         user: "root",
-        password: "Pugluverg51",
+        password: "",
         database: "media_db"
     });
 };
@@ -27,7 +27,7 @@ module.exports = connection;
 //   port: 3306,
 //   host: "localhost",
 //   user: "root",
-//   password: "Pugluverg51",
+//   password: "",
 //   database: "media_db"
 // });
 
